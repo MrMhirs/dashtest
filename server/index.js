945bd276611f1f79af07df709205f9258aa4c637
RB <87038824+MrMhirs@users.noreply.github.com>
2022-12-18 13:32:36 +0100

    Backend Setup Connection to DB

--- server/index.js
+++ server/index.js
@@ -11,20 +11,33 @@ import managementRoutes from "./routes/management.js";
 import salesRoutes from "./routes/sales.js";
 
 // ---------------- CONFIG ------------------
+mongoose.set('strictQuery', true);
 dotenv.config();
 const app = express();
-app.use(express.json())
+app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin"}));
-app.use(morgan("common"))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
-
-
 
 // ---------------- ROUTES ------------------
 app.use("/client", clientRoutes);
 app.use("/general", generalRoutes);
 app.use("/management", managementRoutes);
 app.use("/sales", salesRoutes);
+
+// ---------------- MONGOOSE SETUP ------------------
+const PORT = process.env.PORT || 9000;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    
+
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Connected to Server Port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
