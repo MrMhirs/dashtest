631b64eae7eb99a313d63d4cc49aa44d28b15d9f
RB <87038824+MrMhirs@users.noreply.github.com>
2023-01-09 19:23:26 +0100

    Transaction Regex

--- server/index.js
+++ server/index.js
@@ -14,7 +14,13 @@ import salesRoutes from "./routes/sales.js";
 import User from "./models/User.js";
 import Product from "./models/Product.js";
 import ProductStat from "./models/ProductStat.js";
-import { dataUser, dataProduct, dataProductStat } from "./data/index.js";
+import {
+  dataUser,
+  dataProduct,
+  dataProductStat,
+  dataTransaction,
+} from "./data/index.js";
+import Transaction from "./models/Transaction.js";
 
 // ---------------- CONFIG ------------------
 mongoose.set("strictQuery", true);
@@ -43,10 +49,11 @@ mongoose
   })
   .then(() => {
     app.listen(PORT, () => console.log(`Connected to Server Port: ${PORT}`));
-    
+
     // ---------------- Only Add data one time ------------------
     // Product.insertMany(dataProduct);
     // ProductStat.insertMany(dataProductStat);
+    // Transaction.insertMany(dataTransaction);
     // User.insertMany(dataUser);
   })
   .catch((error) => console.log(`${error} did not connect`));
