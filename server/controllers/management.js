852621d49cb6e443dd1ea320edf75c6c1a40ff36
RB <87038824+MrMhirs@users.noreply.github.com>
2023-01-13 12:32:32 +0100

    Management WIP

--- server/controllers/management.js
+++ server/controllers/management.js
@@ -1,10 +1,25 @@
 import mongoose from "mongoose";
 import User from "../models/User.js";
+import Transaction from "../models/Transaction.js";
+
+
 
 export const getAdmins = async (req, res) => {
     try {
         const admins = await User.find ({ role: "admin"}).select("-password");
         res.status(200).json(admins);
+    } catch (error) {
+        res.status(404).json({ message: error.message})
+    }
+};
+
+
+export const getUserPerformance = async (req, res) => {
+    try {
+        const {id} = req.params;
+
+        const userWithStats =  await User.aggregate
+
     } catch (error) {
         res.status(404).json({ message: error.message})
     }
